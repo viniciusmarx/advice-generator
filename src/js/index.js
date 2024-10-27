a6d2c6cc7e3d8c4e4916d2b5362ca2a5e813b3fb
vinicius marx <vinimarx@live.com>
2024-10-26 21:04:08 -0300

    ✨ add api consumption

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,22 @@
 const adviceNumber = document.getElementById("advice-number");
 const advice = document.getElementById("advice");
 const button = document.getElementById("button");
-let number = 1;
 
-button.addEventListener("click", () => {
-	number++;
-	adviceNumber.textContent = `ADVICE #${number}`;
-});
+async function getAdvice() {
+	try {
+		const response = await fetch("https://api.adviceslip.com/advice");
+		if (!response.ok) {
+			throw new Error("Error in api response");
+		}
+		const { slip } = await response.json();
+		adviceNumber.textContent = `ADVICE #${slip.id}`;
+		advice.textContent = slip.advice;
+	} catch (error) {
+		advice.textContent = "Sorry, we are unable to obtain advice at this time.";
+		console.error(`Erro ao fazer a requisição: ${error}`);
+	}
+}
+
+button.addEventListener("click", getAdvice);
+
+getAdvice();
